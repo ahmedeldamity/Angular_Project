7004fd25796ba3501f10b80904fb5e680377e153
ahmedeldamity <ahmedeldamity25@gmail.com>
2023-12-29 08:40:31 +0200

    Angular life cycle

--- src/app/Directives/light-box.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LightBoxDirective } from './light-box.directive';
-
-describe('LightBoxDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LightBoxDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/Directives/light-box.directive.ts
+++ src/app/Directives/light-box.directive.ts
@@ -1,29 +1,34 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[LightBox]'
 })
-export class LightBoxDirective {
+export class LightBoxDirective implements OnChanges{
 
   // if you take this variable from html
   // you must add property binding Input() before it
   @Input() mouseOverColor:string = "blue";
   @Input() mouseOutColor:string = "blue";
+  @Input() defaultColor:string = "blue";
 
   constructor(public elementRef: ElementRef)
   {
-    elementRef.nativeElement.style.border="5px solid black"
+
+  }
+  ngOnChanges(): void
+  {
+    this.elementRef.nativeElement.style.border=`5px solid ${this.defaultColor}`;
   }
 
   @HostListener('mouseover')
   onMouseOver()
   {
-    this.elementRef.nativeElement.style.border=`5px solid ${this.mouseOverColor}`
+    this.elementRef.nativeElement.style.border=`5px solid ${this.mouseOverColor}`;
   }
 
   @HostListener('mouseout')
   onMouseOut()
   {
-    this.elementRef.nativeElement.style.border=`5px solid ${this.mouseOutColor}`
+    this.elementRef.nativeElement.style.border=`5px solid ${this.mouseOutColor}`;
   }
 }
